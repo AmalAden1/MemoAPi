a4893706d97dd019da2917d27f5b7f1badf6b733
Amal Aden <amal.aden@student.careeria.fi>
2024-11-13 13:23:29 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MemoAPi.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MemoAPi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Memo> Memos { get; set; }
+    }
+}
---
+++ Models/Memo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+namespace MemoAPi.Models
+{
+    public class Memo
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+
+        public string Note { get; set; }
+        [Required]
+        
+        public bool Important { get; set; }
+
+        public DateTime Created { get; set; }
+
+    }
+}
